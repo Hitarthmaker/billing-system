1fb3f809034ac601f85221937da4f43a1c6664b5
Hitarth <157163473+Hitarthmaker@users.noreply.github.com>
2025-10-15 14:44:00 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from extensions import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+class Bill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total = db.Column(db.Float, nullable=False)
